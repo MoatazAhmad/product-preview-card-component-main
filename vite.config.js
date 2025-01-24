46dae061f74105099726c1cb632af4daa569f8dd
MOATAZ SApry <you@example.com>
2025-01-24 11:08:24 +0200

    build the project

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import pugPlugin from "vite-plugin-pug"; // Vite plugin for Pug support
 // import tailwindcss from "tailwindcss";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [pugPlugin(), tailwindcss()],
+  plugins: [tailwindcss()],
 });
